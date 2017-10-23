87bf04bad302d47cd91b4bd530e9ceb2a3fb1f78
Rodrigo Gomes Dias <rodrigogmdias@gmail.com>
2017-10-23 13:16:27 -0200

    Publish

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -15,13 +15,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
+import Login from 'containers/Login/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 export default function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/criaprova-web/" component={HomePage} />
+        <Route exact path="/criaprova-web/login" component={Login} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
---
+++ app/containers/Login/index.js
@@ -0,0 +1,55 @@
+/**
+ *
+ * Login
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+import { Grid } from 'react-bootstrap';
+
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import makeSelectLogin from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+import messages from './messages';
+
+export class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <h1>
+        <FormattedMessage {...messages.header} />
+      </h1>
+    );
+  }
+}
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  login: makeSelectLogin(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+const withReducer = injectReducer({ key: 'login', reducer });
+const withSaga = injectSaga({ key: 'login', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(Login);
